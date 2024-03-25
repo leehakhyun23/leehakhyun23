c8ccea44efa5b870762b343450dbaaf765d4f7f9
DESKTOP-F4BC13L\hi <gkrckdgus@naver.com>
2024-03-25 15:19:34 +0900

    3/25 switch문

--- 02_JAVA_pratice/chop04-control-flow-lecture-source/src/com/ohgirafferes/section01/conditional/Application1.java
+++ 02_JAVA_pratice/chop04-control-flow-lecture-source/src/com/ohgirafferes/section01/conditional/Application1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Application1 {
     public static void main(String[] args) {
-        C_ifElseif a = new C_ifElseif();
-        a.testNestedIfElse();
+        D_swtich a = new D_swtich();
+        a.testMachine();
 
     }
 }
