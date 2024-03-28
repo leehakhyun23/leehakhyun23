19b44ebe3451f36b003c53ba4b4aac8e27abe731
DESKTOP-F4BC13L\hi <gkrckdgus@naver.com>
2024-03-28 13:20:05 +0900

    3/28 클래스 캡슐화 추상화

---
+++ 01-lecture-source/chap06-class-and-object-lecture-sourece/src/com/ohgiraffers/section02/encapsulation/problem2/Monster.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section02.encapsulation.problem2;
+
+public class Monster {
+    //String name;
+    //int hp;
+    /*
+    * name 필드의 이름을 kinds 로 변경한다*/
+    String kinds;
+    int hp;
+}
