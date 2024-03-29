72059624c21d2661469e7b770a0d71048f488881
DESKTOP-F4BC13L\hi <gkrckdgus@naver.com>
2024-03-29 15:14:26 +0900

    3-29 다양한 변수

---
+++ 01-lecture-source/chap06-class-and-object-lecture-sourece/src/com/ohgiraffers/section07/Kindsofvariable/Application.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section07.Kindsofvariable;
+
+public class Application {
+    public static void main(String[] args) {
+        KindoOfVariable kd = new KindoOfVariable();
+        kd.testMethod(200);
+        kd.testMethod();
+    }
+}
