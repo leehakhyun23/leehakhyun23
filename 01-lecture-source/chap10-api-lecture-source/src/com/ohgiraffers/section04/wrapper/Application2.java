07d77653edba1f2c65da5775e0685566c91bbac5
DESKTOP-F4BC13L\hi <gkrckdgus@naver.com>
2024-04-03 17:08:42 +0900

    4/3 캘린더, time

---
+++ 01-lecture-source/chap10-api-lecture-source/src/com/ohgiraffers/section04/wrapper/Application2.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section04.wrapper;
+
+public class Application2 {
+    public static void main(String[] args) {
+        /* 문자열 파싱(parsing)에 대해 이해할 수 있다.
+        *
+        * parsing : 문자열(String)값을 기본 자료형 값으로 변경하는 것을 parsing이라고 한다
+        *  */
+
+        byte b = Byte.parseByte("1");
+        short s = Short.parseShort("2");
+        int i = Integer.parseInt("4");
+        long l = Long.parseLong("8"); //8L은 안 됨
+        float f = Float.parseFloat("4.0f");
+        double d = Double.parseDouble("8");
+        boolean b1 = Boolean.parseBoolean("true");
+        char c= "abc".charAt(0);
+
+    }
+}
